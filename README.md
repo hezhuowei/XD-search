018d538aae2a67180a233120c5a75b5afb117f34
hezhu <harold@ubuntu.ubuntu-domain>
2018-01-09 21:08:15 +0800

    修复cache条目删除后整站无法缓存的问题

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 # something-in-one
 ------
 
-面向手机的搜索引擎导航。
+适配手机的搜索引擎导航，单页webapp，默认注册serviceworker以开启cache，只有第一次需要网络加载本页。
 
 #### demo
 http://www.binaryio.top
-
-------
-主要适配手机浏览器,页面设了离线缓存，理论加载一次之后都可以秒开。
-
 ------
 ~~Bootstrap~~ +vue.js+Jquery
 
 ## Note 
 
 *出于安全考量，Service workers只能由HTTPS承载，毕竟修改网络请求的能力暴露给中间人攻击会非常危险。*
-Service workers需要HTTPS才能成功注册,所以启用缓存需要部署HTTPS。
+Service workers需要HTTPS才能成功注册,所以启用cache需要部署HTTPS。
--- sw.js
+++ sw.js
@@ -1,46 +1,29 @@
-this.addEventListener('install', (event) => {
+const cacheList = [
+    '/',
+    '/index.html',
+    '/js/jquery-3.2.1.min.js',
+    '/js/vue.min.js'
+]
+self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('v1').then((cache) => {
-            return cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/js/jquery-3.2.1.min.js',
-                    '/js/vue.min.js'
-                ]
+            return cache.addAll(cacheList
             )
         })
-    )
+    );
 });
-this.addEventListener('fetch', (event) => {
-    if (event.request.url===self.location.origin+'/update.html')
-    {
-        console.log("update cache");
-        caches.open('v1').then((cache) => {
-            console.log("updated")
-            return cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/js/jquery-3.2.1.min.js',
-                    '/js/vue.min.js'
-                ]
-            )
-        })
-
-    }
-    else {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            if (response) {
-                console.log('load from cache ' + event.request.url);
-                return response;
-            }
-            else {
-              return  fetch(event.request).then((response) => {
-                    return response;
-                }).catch((err)=>{
-                    console.log('err:'+err.toString());
-                })
-            }
-        })
-    );}
+self.addEventListener('fetch', (event) => {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                if (response) {
+                    return response
+                }
+                throw Error('The cached response that was expected is missing.')
+            }).catch(() => {
+                return fetch(event.request)
+            })
+        );
+});
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
 });
